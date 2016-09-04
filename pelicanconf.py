ea4656e1359d2d075d3dd4e18d67d6bdd3476014
Ryan Melvin <melvrl13@wfu.edu>
2016-09-04 18:03:29 -0400

    Rodin feelings

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,19 +26,46 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'http://twitter.com/odsap'),
+          ('linkedin', 'http://www.linkedin.com/in/ryan-melvin-834a8522'),
+          ('github', 'http://github.com/melvrl13'),
+          )
+
+GITHUB_USER = 'melvrl13'
+TWITTER_USERNAME = 'odsap'
+TWITTER_CARDS = True
+
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
 MARKUP = ('md', 'ipynb')
 
+# Plugins and their necessities
 PLUGIN_PATHS = ['./pelican-plugins']
-PLUGINS = ['ipynb.markup', 'tipue_search', 'neighbors', 'related_posts']
+PLUGINS = ['ipynb.markup', 'sitemap', 'tipue_search', 'related_posts', 'tag_cloud', 'liquid_tags.notebook']
+DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'search')
+RELATED_POSTS_MAX = 10
+DISPLAY_ARTICLE_INFO_ON_INDEX = True
+DISPLAY_TAGS_ON_SIDEBAR = True
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
 
-THEME = '/home/melvrl13/PycharmProjects/odsap/pelican-themes/plumage'
+THEME = '/home/melvrl13/PycharmProjects/odsap/pelican-themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'cerulean'
+PYGMENTS_STYLE = 'solarizeddark'
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
 
-TIPUE_SEARCH = True
+LOAD_CONTENT_CACHE = False
